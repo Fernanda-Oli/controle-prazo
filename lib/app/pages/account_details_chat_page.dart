cf0160a1982a7578e83c95c76ade3066c7428892
FernandaOli <nanda.oliveir@hotmail.com>
2023-09-24 21:20:09 -0300

    add menu for interaction in the AccountDetailChatPagState toolbar

--- lib/app/pages/account_details_chat_page.dart
+++ lib/app/pages/account_details_chat_page.dart
@@ -16,11 +16,43 @@ class _AccountDetailChatPagStateState extends State<AccountDetailChatPagState> {
         child: Scaffold(
       appBar: AppBar(
         title: Text(widget.nameClient.toString()),
-          backgroundColor: Colors.indigoAccent,
-          actionsIconTheme: const IconThemeData.fallback()),
+        backgroundColor: Colors.indigoAccent,
+        actionsIconTheme: const IconThemeData.fallback(),
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (String choice) {
+              if (choice == 'Item 1') {
+                // Lidar com a seleção do Item 1
+              } else if (choice == 'Item 2') {
+                // Lidar com a seleção do Item 2
+              }
+              // Adicione mais opções, se necessário
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem<String>(
+                  value: 'Item 2',
+                  child: Text('Item 2'),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
       body: Container(
         color: Colors.indigo[100],
       ),
     ));
   }
 }
+
+  List<Widget> windowMenuItem(List itens) {
+    return itens.map((item) {
+      return PopupMenuItem<String>(
+      value: item,
+      child: Text(item),
+    );
+    }).toList();
+  
+  }
+
